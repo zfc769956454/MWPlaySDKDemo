b2233ae9e9cd380a169f76ce2458923f11a4b7cd
fanzq <3029778489@qq.com>
2018-12-25 11:08:53 +0800

    更新Model

---
+++ MWPlaySDKDemo/MWPlaySDKDemo/Home/Model/MWLiveDetailModel.m
@@ -0,0 +1,16 @@
+//
+//Created by ESJsonFormatForMac on 18/11/27.
+//
+
+#import "MWLiveDetailModel.h"
+
+@implementation MWLiveDetailModel
+
+
++ (NSDictionary *)mj_replacedKeyFromPropertyName{
+    return @{@"liveId":@"id"};
+}
+
+@end
+
+
